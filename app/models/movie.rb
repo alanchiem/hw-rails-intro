c92352811a6ab37d88b87a60e202b741d54c281a
Alan Chiem <alanchiem@berkeley.edu>
2022-10-06 06:34:54 +0000

    removed checkbox html

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,9 @@ class Movie < ActiveRecord::Base
     # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
     #  movies with those ratings
     # if ratings_list is nil, retrieve ALL movies
+    if ratings_list == nil
+      
     end
-    
+  end
+
 end
